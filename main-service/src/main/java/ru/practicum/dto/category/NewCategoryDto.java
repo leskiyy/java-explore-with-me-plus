d50d882c9af251c5d05f14bc32d41e75dcddce27
Alexey <leskiy@yandex.ru>
2025-06-16 23:46:28 +0300

    add admin_categories

--- main-service/src/main/java/ru/practicum/dto/category/NewCategoryDto.java
+++ main-service/src/main/java/ru/practicum/dto/category/NewCategoryDto.java
@@ -1,13 +1,17 @@
 package ru.practicum.dto.category;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AccessLevel;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class NewCategoryDto {
+    @NotBlank
     @Size(min = 1, max = 50)
     String name;
 }
--- main-service/src/main/java/ru/practicum/service/CategoryService.java
+++ main-service/src/main/java/ru/practicum/service/CategoryService.java
@@ -1,7 +1,40 @@
 package ru.practicum.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.practicum.dto.category.CategoryDto;
+import ru.practicum.dto.category.NewCategoryDto;
+import ru.practicum.entity.Category;
+import ru.practicum.exception.NotFoundException;
+import ru.practicum.mapper.CategoryMapper;
+import ru.practicum.repository.CategoryRepository;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper mapper;
+
+    public CategoryDto save(NewCategoryDto dto) {
+        Category saved = categoryRepository.save(mapper.toEntity(dto));
+        return mapper.toDto(saved);
+    }
+
+    public void deleteCategory(Integer catId) {
+        if (!categoryRepository.existsById(catId)) {
+            throw new NotFoundException("Category with id=" + catId + " was not found");
+        }
+        categoryRepository.deleteById(catId);
+        categoryRepository.flush();
+    }
+
+
+    public CategoryDto update(CategoryDto dto) {
+        if (!categoryRepository.existsById(dto.getId())) {
+            throw new NotFoundException("Category with id=" + dto.getId() + " was not found");
+        }
+        Category updated = categoryRepository.saveAndFlush(mapper.toEntity(dto));
+        return mapper.toDto(updated);
+    }
 }
